c2a424ce75376c097d484ec28f5fd0bf7ad15473
zh <994715379@qq.com>
2020-11-14 19:33:25 +0800

    添加Train类

---
+++ src/main/java/com/bjpowernode/github/Train.java
@@ -0,0 +1,15 @@
+package main.java.com.bjpowernode.github;
+
+/**
+ * @ClassName
+ * @Description 一句话描述此类或方法的作用
+ * @Author PowerNode
+ * @Date 2020/11/14 19:32
+ * @Version 1.0
+ */
+public class Train {
+    private String name;
+    private int age;
+    private String loc;
+
+}
