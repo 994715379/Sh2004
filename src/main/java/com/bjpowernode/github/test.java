252653d9d2a4dcd878e64da0ec32c333b96a6c4b
zh <994715379@qq.com>
2020-11-14 16:46:39 +0800

    第一次提交

---
+++ src/main/java/com/bjpowernode/github/test.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.github;
+
+/**
+ * @ClassName
+ * @Description 一句话描述此类或方法的作用
+ * @Author PowerNode
+ * @Date 2020/11/14 16:07
+ * @Version 1.0
+ */
+public class test {
+    private String id;
+
+}
