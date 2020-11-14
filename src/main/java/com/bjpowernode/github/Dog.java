ab154b57cc5882097501bd453566a57510a37e17
zh <994715379@qq.com>
2020-11-14 19:41:45 +0800

    添加狗类

---
+++ src/main/java/com/bjpowernode/github/Dog.java
@@ -0,0 +1,13 @@
+package main.java.com.bjpowernode.github;
+
+/**
+ * @ClassName
+ * @Description 一句话描述此类或方法的作用
+ * @Author PowerNode
+ * @Date 2020/11/14 19:40
+ * @Version 1.0
+ */
+public class Dog {
+    private String names;
+
+}
